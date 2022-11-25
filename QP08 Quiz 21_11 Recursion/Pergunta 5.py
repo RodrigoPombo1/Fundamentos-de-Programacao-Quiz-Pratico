60a884a299b5029d6c8fdc3da40d941bb8adbad5
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-11-25 19:23:59 +0000

    pergunta 5 finalmente

---
+++ QP08 Quiz 21_11 Recursion/Pergunta 5.py
@@ -0,0 +1,39 @@
+def soup(matrix, word):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            #easiest case (where the word is only one letter)
+            if matrix[i][j] == word:
+                return str(chr(i + 65)) + str(j)
+            #find the position of the first character of the word
+            if matrix[i][j] == word[0]:
+                if soup2(matrix, word[1:], i, j):
+                    return str(chr(i + 65)) + str(j + 1)
+
+
+def soup2(matrix, word, line, column):
+    
+    #caso base (em q se deve parar)
+    if word == "":
+        return True
+    
+    #verificar linha acima, linha abaixo, coluna a esquerda coluna a direita
+    if line != 0:
+        #verificar cima
+        if matrix[line - 1][column] == word[0]:
+            return soup2(matrix, word[1:], line - 1, column)
+    
+    if line != len(matrix) - 1:
+        #verificar baixo
+        if matrix[line + 1][column] == word[0]:
+            return soup2(matrix, word[1:], line + 1, column)
+    
+    if column != 0:
+        #verificar esquerda
+        if matrix[line][column - 1] == word[0]:
+            return soup2(matrix, word[1:], line, column - 1)
+        
+    if column != len(matrix[0]) - 1:
+        #verificar direita
+        if matrix[line][column + 1] == word[0]:
+            return soup2(matrix, word[1:], line, column + 1)
+    return False
